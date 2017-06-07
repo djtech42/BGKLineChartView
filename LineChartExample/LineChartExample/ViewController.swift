ac018a92c13f8fc3c1f57faec1d9a7f2a536af6b
Dan Turner <dan.turner@madebyatomicrobot.com>
2017-06-07 15:01:42 -0400

    update example

--- LineChartExample/LineChartExample/ViewController.swift
+++ LineChartExample/LineChartExample/ViewController.swift
@@ -8,61 +8,72 @@
 
 import UIKit
 import BGKLineChartView
+import FuncTools
 
 class ViewController: UIViewController {
-
+    // ------------------------------- IBOutlets -----------------------------------
     @IBOutlet weak var lineChart: BGKLineChartView!
     
+    // ------------------------------- Model Data -----------------------------------
+    lazy var modelData: [BGKChartObject] = {
+        var mergedValues: [BGKChartObject] = []
+        
+        let sampleGroups = (
+            first: [1, 3, 5, 7, 9],
+            second: [2, 4, 6, 8, 10]
+        )
+        
+        var lines: (BGKChartObject, BGKChartObject, BGKChartObject) = ([], [], [])
+        
+        for (index, sample) in sampleGroups.first.enumerated() {
+            let point = BGKBasicPoint(
+                xValue: Double(sample),
+                yValue: Double(sampleGroups.second[index])
+            )
+            let otherPoint = BGKBasicPoint(
+                xValue: Double(sampleGroups.second[index] * 4),
+                yValue: Double(sample * 2)
+            )
+            
+            lines.0.append(point)
+            lines.1.append(otherPoint)
+        }
+        
+        lines.2 = ExpandedFunction({ sin($0) * 5 }, overRange: 0..<30).points
+        
+        mergedValues.append(lines.0)
+        mergedValues.append(lines.1)
+        mergedValues.append(lines.2)
+        
+        return mergedValues
+    }()
+    
+    
+    // ------------------------------- Connect Data Source -----------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        setupData()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // Helper method to setup data
-    func setupData() {
-        
-        struct SampleData: BGKChartPoint {
-            var xValue: Double
-            var yValue: Double
-        }
-        
-        let samples = [1, 3, 5, 7, 9]
-        let samplesTwo = [2, 4, 6, 8, 10]
-        var line1: BGKChartObject = []
-        var line2: BGKChartObject = []
-        for (index, sample) in samples.enumerated() {
-            let point = SampleData(xValue: Double(sample), yValue: Double(samplesTwo[index]))
-            let otherPoint = SampleData(xValue: Double(samplesTwo[index] * 4), yValue: Double(sample * 2))
-            line1.append(point)
-            line2.append(otherPoint)
-        }
-        values.append(line1)
-        values.append(line2)
         lineChart.dataSource = self
     }
-    
-    var values: [BGKChartObject] = []
 }
+
 
 extension ViewController: BGKLineChartDataSource {
-    // DataSource Conformance
+    // ------------------------------- Data -----------------------------------
     func chartItems(in lineChartView: BGKLineChartView) -> [BGKChartObject] {
-        return values
+        return modelData
     }
-}
-
-extension ViewController {
+    
+    
+    // ------------------------------- Style -----------------------------------
     func style(for lineNumber: Int, in lineChartView: BGKLineChartView) -> BGKLineStyle? {
         return BGKLineStyle(withThickness: 2.0, andColor: .black)
     }
     
-    func labelMode(for lineChartView: BGKLineChartView) -> BGKLabelMode {
-        return .minMax(withLimitedNumberOfDecimalDigits: 2)
+    func style(for lineChartView: BGKLineChartView) -> BGKChartStyle? {
+        return BGKChartStyle(withBackgroundColor: .lightGray, andShowingAxes: true, withLabelMode: .minMax(withLimitedNumberOfDecimalDigits: 2))
+    }
+    
+    func chartExtentsShouldBePadded(_ lineChartView: BGKLineChartView) -> Bool {
+        return false
     }
 }
